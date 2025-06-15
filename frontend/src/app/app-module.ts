4e88366bbcde5ac4d8af84a542197d17959aacf3
CNAnda1623 <cnanda162@gmail.com>
2025-06-15 14:14:34 +0530

    frontend

---
+++ frontend/src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Navbar } from './component/navbar/navbar';
+import { Footer } from './component/footer/footer';
+
+@NgModule({
+  declarations: [
+    App,
+    Navbar,
+    Footer
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
