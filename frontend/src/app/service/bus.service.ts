6210c8bd13c3bab603d214bf751d086c3e03eb26
CNAnda1623 <cnanda162@gmail.com>
2025-06-27 16:35:57 +0530

    url changes

--- frontend/src/app/service/bus.service.ts
+++ frontend/src/app/service/bus.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BusModel } from "../model/bus.model"
-import { url } from '../config';
+import { url } from '../config/index';
 import { BookingModel } from '../model/booking.model';
 
 @Injectable({
